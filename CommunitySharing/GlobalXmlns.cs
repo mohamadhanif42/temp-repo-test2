0801700c69c96c4f9286b5630124f2ed0ab249fb
Mohamad Hanif <bcs23020031@student.uts.edu.my>
2025-12-21 12:31:15 +0800

    Delete CommunitySharing directory

--- CommunitySharing/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CommunitySharing")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CommunitySharing.Pages")]
--- CommunitySharing/Resources/Styles/AuthStyles.xaml.cs
+++
@@ -1,8 +0,0 @@
-namespace CommunitySharing.Resources.Styles; 
-
-public partial class AuthStyles : ResourceDictionary
-{
-    public AuthStyles()
-    {
-    }
-}
